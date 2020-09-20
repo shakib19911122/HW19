daa4bb3f28c53fc1b050b8068ce7a35af1decc5f
shakib <shakib1122@gmail.com>
2020-09-21 07:36:34 +1000

    initial upload

---
+++ employee-directory/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "./components/Header";
+import SearchBar from "./components/SearchBar"
+
+function App() {
+  return (
+    <div className="App">
+   
+     <Header />
+     <SearchBar />
+     
+   
+    </div>
+  );
+}
+
+export default App;
+
---
+++ employee-directory/src/components/SearchBar/index.js
@@ -0,0 +1,27 @@
+
+
+import React from "react";
+
+
+function SearchBar() {
+    return (
+        <div className="input-group">
+            <div className="input-group-prepend">
+                <span className="input-group-text">Search</span>
+            </div>
+            <textarea className="form-control" aria-label="With textarea"></textarea>
+        </div>
+
+
+    )
+}
+
+export default SearchBar;
+
+
+
+
+
+
+
+
