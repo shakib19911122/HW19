2f86d8e256538b98bdffb8518df252e095fcb99b
shakib <shakib1122@gmail.com>
2020-09-22 21:49:09 +1000

    Updates

--- employee-directory/src/utils/API.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-// Export an object containing methods we'll use for accessing the Dog.Ceo API
-
-export default {
-  getUsers: function() {
-    return axios.get("https://randomuser.me/api/?results=200&nat=AU");
-  }
-};
---
+++ precache-manifest.c2b671c9d94e1c9f3f97fbb148a002b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eacf9f40cca1eedbc1795a4c9faa6a78",
+    "url": "/employee-directory/index.html"
+  },
+  {
+    "revision": "83cc41f04df6326cfa15",
+    "url": "/employee-directory/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "83cc41f04df6326cfa15",
+    "url": "/employee-directory/static/js/2.6250250e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/employee-directory/static/js/2.6250250e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03ed0dc90456fec13aeb",
+    "url": "/employee-directory/static/js/main.281a2114.chunk.js"
+  },
+  {
+    "revision": "79d7c4f4ad2a4d373d4e",
+    "url": "/employee-directory/static/js/runtime-main.759497ab.js"
+  }
+]);
