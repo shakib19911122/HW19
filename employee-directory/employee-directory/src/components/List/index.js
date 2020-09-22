79617952b2b32a8a847ff444c658b1125de19fdb
shakib <shakib1122@gmail.com>
2020-09-22 23:12:59 +1000

    upload changes

---
+++ employee-directory/employee-directory/src/components/List/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import DataArea from "../DataArea/index";
+
+
+function List (){
+    return (
+        <>
+        <DataArea />
+        </>
+    );
+};
+
+export default List;
