2ae4f3165b212676d17238846af1fa5024a7b5f3
shakib <shakib1122@gmail.com>
2020-09-23 17:56:29 +1000

    Updates

---
+++ precache-manifest.b940ce1586212baef6d5978c4a789eee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f47995a218d50f5cd42852fdad50cbe6",
+    "url": "/HW19/index.html"
+  },
+  {
+    "revision": "83cc41f04df6326cfa15",
+    "url": "/HW19/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "83cc41f04df6326cfa15",
+    "url": "/HW19/static/js/2.6250250e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/HW19/static/js/2.6250250e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a24e718e07fa469611ea",
+    "url": "/HW19/static/js/main.a50b8f1a.chunk.js"
+  },
+  {
+    "revision": "051057e7bf6a5cdd8e95",
+    "url": "/HW19/static/js/runtime-main.b7d9f714.js"
+  }
+]);
